5543c43314d03b12aace36a39bb83cb5ee3dee3a
Deepak Vishwakarma <79501955+beliver-jsx@users.noreply.github.com>
2023-05-23 16:21:32 +0530

    Update register.ts

--- src/features/authentication/services/register.ts
+++ src/features/authentication/services/register.ts
@@ -4,11 +4,11 @@ import strapi from "~/utils/strapi";
 interface type extends StrapiRegistrationData {
   email: string;
   password: string;
-  firstName:string;
-  lastName?:string
-  phone:string
+  firstName: string;
+  lastName?: string
+  phone: string
 }
-export const createUser = async (userData: type) => {
+export const createUser = async (userData: any) => {
   try {
     const {
       password,
@@ -17,19 +17,19 @@ export const createUser = async (userData: type) => {
       lastName,
       phone
     } = userData;
-    
+
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const { user, jwt } = await strapi.register({
       email: email,
       username: phone,
       password: password,
-      firstname:firstName,
-      lastname:lastName,
-      phone_number:phone
+      firstname: firstName,
+      lastname: lastName,
+      phone_number: phone
     } as any);
 
     return { user, password };
-   
+
   } catch (error: any) {
     console.log(error);
     // eslint-disable-next-line @typescript-eslint/restrict-template-expressions, @typescript-eslint/no-unsafe-member-access
